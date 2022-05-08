d220f0561a6b5617b54ee13e593755ecdb169eb1
iremsonmezgul <sonmezgull.irem@gmail.com>
2022-05-09 01:58:40 +0300

    Admin Template Implemented

---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,10 @@
+<div class="footer-wrap pd-20 mb-20 card-box">
+    DeskApp - Bootstrap 4 Admin Template By <a href="https://github.com/dropways" target="_blank">Ankit Hingarajiya</a>
+</div>
+</div>
+</div>
+<!-- js -->
+<script src="{{asset('assets')}}/admin/vendors/scripts/core.js"></script>
+<script src="{{asset('assets')}}/admin/vendors/scripts/script.min.js"></script>
+<script src="{{asset('assets')}}/admin/vendors/scripts/process.js"></script>
+<script src="{{asset('assets')}}/admin/vendors/scripts/layout-settings.js"></script>
