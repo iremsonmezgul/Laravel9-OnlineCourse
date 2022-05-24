9417711f2a76f277d56201db9591084c36feb964
iremsonmezgul <sonmezgull.irem@gmail.com>
2022-05-24 20:42:22 +0300

    One to Many Relationships Category and Courses

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     use HasFactory;
+    #many to one
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
